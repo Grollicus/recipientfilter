a6029bdab5c32d170be96f3011f111b1ff971b47
Grollicus <Grollicus@users.noreply.github.com>
2019-05-24 00:01:24 +0200

    postfix_policy library, handle communication as bytes

--- recipient_filter/src/main.rs
+++ recipient_filter/src/main.rs
@@ -8,24 +8,24 @@ extern crate sha2;
 extern crate toml;
 extern crate users;
 
+extern crate postfix_policy;
+use postfix_policy::{handle_connection, PolicyRequestHandler, PolicyResponse};
+
 use hmac::{Hmac, Mac};
 use nix::unistd::{setresgid, setresuid, Gid, Uid};
-use regex::Regex;
+use regex::bytes::Regex;
 use serde::Deserialize;
 use sha2::Sha256;
 
-use std::collections::HashMap;
 use std::env;
 use std::ffi::{OsStr, OsString};
 use std::fs::{metadata, remove_file, set_permissions, File};
 use std::io::BufReader;
 use std::os::unix::fs::{FileTypeExt, PermissionsExt};
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::net::UnixListener;
+use std::thread;
 
 // TODO whiltelist / blacklist
-// TODO process a Read / Write, not a UnixStream
-// TODO use bytes
-// make PolicyContext an Interface & library
 // config with regex -> username mapping
 
 use std::error::Error;
@@ -33,7 +33,7 @@ use std::io::prelude::*;
 
 const CONFIG_FILE_DEFAULT: &str = "/etc/recipient_filter.toml";
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Config {
     secret: String,
     min_length: usize,
@@ -42,111 +42,88 @@ struct Config {
     group: String,
 }
 
-fn compute_hash(msg: &str, secret: &str) -> String {
-    let mut mac: Hmac<Sha256> = Hmac::new_varkey(secret.as_bytes()).expect("The Secret is invalid");
-    mac.input(msg.as_bytes());
+fn compute_hash(msg: &[u8], secret: &[u8]) -> Vec<u8> {
+    let mut mac: Hmac<Sha256> = Hmac::new_varkey(secret).expect("The Secret is invalid");
+    mac.input(msg);
     let code = mac.result().code();
-    format!("{:x}", code)
+    format!("{:x}", code).into_bytes()
 }
 
 #[test]
 fn test_compute_hash() {
-    let secret = "asdf";
-    let msg = "test";
+    let secret = b"asdf";
+    let msg = b"test";
 
     assert_eq!(
-        "8338a5d6d429c4f6618e2e02e296e283570f46378bc7e2a8dbbb399599cf6096",
-        compute_hash(msg, secret)
+        b"8338a5d6d429c4f6618e2e02e296e283570f46378bc7e2a8dbbb399599cf6096"[..],
+        compute_hash(msg, secret)[..]
     );
 }
 
-struct PolicyContext {
-    attributes: HashMap<String, String>,
+struct EmailValidator<'l> {
+    config: &'l Config,
+    response: Option<PolicyResponse>,
 }
-
-impl PolicyContext {
-    fn new() -> Self {
-        PolicyContext {
-            attributes: HashMap::new(),
+impl<'l> EmailValidator<'l> {
+    fn request(&mut self, policy: &[u8]) {
+        if policy != b"smtpd_access_policy" {
+            println!("Got unknown policy request: {:?}", policy);
+            self.response = Some(PolicyResponse::Dunno);
         }
     }
+    fn recipient(&mut self, recipient: &[u8]) {
+        lazy_static! {
+            static ref MAIL_REGEX: Regex = Regex::new(r"^([^@]+)\.([a-f0-9]+)@.+$").expect("MAIL_REGEX invalid");
+        }
 
-    fn parse_line(&mut self, key: &str, value: &str) {
-        self.attributes
-            .insert(String::from(key), String::from(value));
+        let mail_match = match MAIL_REGEX.captures(recipient) {
+            Some(m) => m,
+            None => {
+                println!("Recipient does not match MAIL_REGEX: {:?}", recipient);
+                self.response = Some(PolicyResponse::Dunno);
+                return;
+            }
+        };
+
+        let recipient_name = mail_match.get(1).expect("MAIL_REGEX has two groups").as_bytes();
+        let recipient_hash = mail_match.get(2).expect("MAIL_REGEX has two groups").as_bytes();
+        if recipient_hash.len() < self.config.min_length {
+            println!("Checking Recipient {:?}: Hash too short!", recipient);
+            self.response = Some(PolicyResponse::Dunno);
+            return;
+        }
+
+        let expected_hash = compute_hash(recipient_name, &self.config.secret.as_bytes());
+        if recipient_hash[0..self.config.min_length] != expected_hash[0..self.config.min_length] {
+            println!("Checking Recipient {:?}: Wrong hash value!", recipient);
+            self.response = Some(PolicyResponse::Dunno);
+            return;
+        }
+
+        self.response = Some(PolicyResponse::Ok)
     }
 }
-
-/*
-request=smtpd_access_policy
-protocol_state=RCPT
-protocol_name=ESMTP
-client_address=131.234.189.14
-client_name=telepax.uni-paderborn.de
-client_port=43528
-reverse_client_name=telepax.uni-paderborn.de
-server_address=12.34.56.78
-server_port=25
-helo_name=telepax.uni-paderborn.de
-sender=foo@some.where.de
-recipient=test@here.net
-recipient_count=0
-queue_id=
-instance=3973.5ce5b0dd.81ed6.0
-size=2004
-etrn_domain=
-stress=
-sasl_method=
-sasl_username=
-sasl_sender=
-ccert_subject=
-ccert_issuer=
-ccert_fingerprint=
-ccert_pubkey_fingerprint=
-encryption_protocol=
-encryption_cipher=
-encryption_keysize=0
-policy_context=
-
-*/
-
-fn handle_request(ctx: PolicyContext, config: &Config) -> String {
-    lazy_static! {
-        static ref MAIL_REGEX: Regex = Regex::new(r"^([^@]+)\.([a-f0-9]+)@.+$").expect("MAIL_REGEX invalid");
-    }
-    println!(
-        "Got Request {}",
-        ctx.attributes.get("request").unwrap_or(&String::from("<MISSING>"))
-    );
-    let recipient = match ctx.attributes.get("recipient") {
-        Some(rcp) => rcp,
-        None => {
-            println!("Recipient missing");
-            return String::from("DUNNO");
+impl<'l> PolicyRequestHandler<'l, Config> for EmailValidator<'l> {
+    fn new(cfg: &'l Config) -> Self {
+        Self {
+            config: cfg,
+            response: None,
         }
-    };
-    let mail_match = match MAIL_REGEX.captures(recipient) {
-        Some(m) => m,
-        None => {
-            println!("Recipient does not match MAIL_REGEX: {:?}", recipient);
-            return String::from("DUNNO");
+    }
+    fn parse_line(&mut self, name: &[u8], value: &[u8]) {
+        if self.response.is_some() {
+            return;
         }
-    };
-
-    let recipient_name = mail_match.get(1).expect("MAIL_REGEX has two groups").as_str();
-    let recipient_hash = mail_match.get(2).expect("MAIL_REGEX has two groups").as_str();
-    if recipient_hash.len() < config.min_length {
-        println!("Checking Recipient {}: Hash too short!", recipient);
-        return String::from("DUNNO");
+        match name {
+            b"request" => self.request(value),
+            b"recipient" => self.recipient(value),
+            _ => {}
+        }
     }
 
-    let expected_hash = compute_hash(recipient_name, &config.secret);
-    if recipient_hash[0..config.min_length] != expected_hash[0..config.min_length] {
-        println!("Checking Recipient {}: Wrong hash value!", recipient);
-        return String::from("DUNNO");
+    fn response(self) -> PolicyResponse {
+        self.response.unwrap_or(PolicyResponse::Dunno)
     }
-
-    String::from("OK")
 }
 
 #[test]
@@ -159,60 +136,25 @@ fn test_handle_request() {
         group: Default::default(),
     };
 
-    let mut ctx = PolicyContext::new();
-    ctx.parse_line("request", "smtpd_access_policy");
-    ctx.parse_line("recipient", "test.8338a5@some.where.net");
-    assert_eq!(handle_request(ctx, &config), "OK");
+    let mut ctx = EmailValidator::new(&config);
+    ctx.parse_line(b"request", b"smtpd_access_policy");
+    ctx.parse_line(b"recipient", b"test.8338a5@some.where.net");
+    assert_eq!(ctx.response(), PolicyResponse::Ok);
 
-    let mut ctx = PolicyContext::new();
-    ctx.parse_line("request", "smtpd_access_policy");
-    ctx.parse_line("recipient", "test.aaaaaa@some.where.net");
-    assert_eq!(handle_request(ctx, &config), "DUNNO");
+    let mut ctx = EmailValidator::new(&config);
+    ctx.parse_line(b"request", b"smtpd_access_policy");
+    ctx.parse_line(b"recipient", b"test.aaaaaa@some.where.net");
+    assert_eq!(ctx.response(), PolicyResponse::Dunno);
 
-    let mut ctx = PolicyContext::new();
-    ctx.parse_line("request", "smtpd_access_policy");
-    ctx.parse_line("recipient", "test@some.where.net");
-    assert_eq!(handle_request(ctx, &config), "DUNNO");
+    let mut ctx = EmailValidator::new(&config);
+    ctx.parse_line(b"request", b"smtpd_access_policy");
+    ctx.parse_line(b"recipient", b"test@some.where.net");
+    assert_eq!(ctx.response(), PolicyResponse::Dunno);
 
-    let mut ctx = PolicyContext::new();
-    ctx.parse_line("request", "smtpd_access_policy");
-    ctx.parse_line("recipient", "test.8338a@some.where.net");
-    assert_eq!(handle_request(ctx, &config), "DUNNO");
-}
-
-fn handle_connection(conn: UnixStream, config: &Config) -> Result<(), Box<Error>> {
-    let mut read = BufReader::new(&conn);
-    let mut ctx = PolicyContext::new();
-
-    loop {
-        let mut resp = String::new();
-        if read.read_line(&mut resp)? == 0 {
-            return Ok(());
-        }
-
-        if resp == "\n" {
-            let action = handle_request(ctx, config);
-            writeln!(&conn, "action={}\n", action)?;
-            ctx = PolicyContext::new();
-            continue;
-        }
-
-        match resp.find('=') {
-            None => {
-                println!("Read invalid line, ignoring: {:?}", resp);
-                continue;
-            }
-            Some(pos) => {
-                let (left, mut right) = resp.split_at(pos);
-                if right.len() < 2 {
-                    println!("Read invalid line, ignoring: {:?}", resp);
-                    continue;
-                }
-                right = &right[1..right.len() - 1];
-                ctx.parse_line(left, right);
-            }
-        }
-    }
+    let mut ctx = EmailValidator::new(&config);
+    ctx.parse_line(b"request", b"smtpd_access_policy");
+    ctx.parse_line(b"recipient", b"test.8338a@some.where.net");
+    assert_eq!(ctx.response(), PolicyResponse::Dunno);
 }
 
 fn main() -> Result<(), Box<Error>> {
@@ -250,10 +192,15 @@ fn main() -> Result<(), Box<Error>> {
     setresuid(uid, uid, uid)?;
 
     for conn in listener.incoming() {
-        let conn = conn?;
-        if let Err(e) = handle_connection(conn, &config) {
-            println!("Error in connection handler: {:?}", e);
-        }
+        let mut conn = conn?;
+        let clone = conn.try_clone()?;
+        let config = config.clone();
+        thread::spawn(move || {
+            let reader = BufReader::new(clone);
+            if let Err(e) = handle_connection::<EmailValidator, _, _, _>(reader, &mut conn, &config) {
+                println!("handle_connection failed: {:?}", e);
+            };
+        });
     }
 
     Ok(())
